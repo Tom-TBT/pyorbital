c50b5ae030363a1544ab1d0a598256eeadad8f80
unknown <tom.boissonnet@hhu.de>
2024-11-14 11:52:09 +0100

    Conversions DMS decimal and constants

--- pyorbital/_modidx.py
+++ pyorbital/_modidx.py
@@ -5,4 +5,9 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://Tom-TBT.github.io',
                 'git_url': 'https://github.com/Tom-TBT/pyorbital',
                 'lib_path': 'pyorbital'},
-  'syms': {'pyorbital.core': {'pyorbital.core.say_hello': ('core.html#say_hello', 'pyorbital/core.py')}}}
+  'syms': { 'pyorbital.conversion': { 'pyorbital.conversion.decimal_to_dms': ('conversion.html#decimal_to_dms', 'pyorbital/conversion.py'),
+                                      'pyorbital.conversion.dms_to_decimal': ('conversion.html#dms_to_decimal', 'pyorbital/conversion.py')},
+            'pyorbital.core': { 'pyorbital.core.HelloSayer': ('core.html#hellosayer', 'pyorbital/core.py'),
+                                'pyorbital.core.HelloSayer.__init__': ('core.html#hellosayer.__init__', 'pyorbital/core.py'),
+                                'pyorbital.core.HelloSayer.say': ('core.html#hellosayer.say', 'pyorbital/core.py'),
+                                'pyorbital.core.say_hello': ('core.html#say_hello', 'pyorbital/core.py')}}}
